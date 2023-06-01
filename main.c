f973ee37d2b83c7d0c01bc5143ee32bd6c9272d1
Alexey Zharikov <leha.zharikov.01@bk.ru>
2023-06-01 21:14:13 +0300

    .

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 #define TESTS_NUM 4
-//#define C 3.701263643715
 #define EPS 10e-14
 #ifndef M_PI
     #define M_PI 3.14159265358979323846
@@ -14,8 +13,8 @@ double antider_right(double x, double h, double m);
 double u(double x, double y);
 double norm(double *v1, double *v2, int size);
 double **create_EwR(double **A, double w, int size);
-void calc_phi(double **R, double **A, double *y, double *b, double *res, double tau, int size); //void <- double*
-void upper_triangle_solve(double **A, double *b, double *x, int size); //
+void calc_phi(double **R, double **A, double *y, double *b, double *res, double tau, int size);
+void upper_triangle_solve(double **A, double *b, double *x, int size);
 void lower_triangle_solve(double **A, double *b, double *x, int size);
 double **create_A(int size);
 double *create_b(double h, int size);
@@ -53,8 +52,6 @@ double **create_EwR(double **A, double w, int size)
     res = (double **)malloc(size * sizeof(double *));
     for (int i = 0; i < size; i++) {
         res[i] = (double *)malloc(size * sizeof(double));
-    }
-    for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             res[i][j] = w * A[i][j];
         }
@@ -97,10 +94,6 @@ void upper_triangle_solve(double **A, double *b, double *x, int size)
 {
     double s;
 
-    for (int i = 0; i < size; i++) {
-        x[i] = 0;
-    }
-
     for (int i = size - 1; i > -1; i--) {
         s = 0;
         for (int j = i + 1; j < size; j++) {
@@ -115,46 +108,32 @@ void lower_triangle_solve(double **A, double *b, double *x, int size)
     double s;
 
     for (int i = 0; i < size; i++) {
-        x[i] = 0;
-    }
-
-    for (int i = 0; i < size; i++) {
         s = 0;
         for (int j = 0; j < i; j++) {
             s += A[i][j] * x[j];
         }
         x[i] = (b[i] - s) / A[i][i];
-        //printf("%lf ||||| %lf", b[i], A[i][i]);
     }
 }
 
 double **create_A(int size)
 {
-    double **res;
     double N = size * size;
+    double **res = (double **)malloc(N * sizeof(double *));
     int i1, i2, j1, j2;
-    res = (double **)malloc(N * sizeof(double *));
+
     for (int i = 0; i < N; i++) {
         res[i] = (double *)malloc(N * sizeof(double));
-    }
-    for (int i = 0; i < N; i++) {
-        i1 = i / size;
+        i1 = i / size; 
         i2 = i % size;
         for (int j = 0; j < N; j++) {
             j1 = j / size;
             j2 = j % size;
-            switch(abs(i1 - j1) + abs(i2 - j2)) {
-                case 0: 
-                    res[i][j] = 8./3;
-                    break;
-                case 1:
-                    res[i][j] = 2./3;
-                    break;
-                case 2:
-                    if (abs(i1 - j1) == 1) {
-                        res[i][j] = 0;
-                    }
-            } 
+            if (abs(i1 - j1) + abs(i2 - j2) == 0) {
+                res[i][j] = 8./3;
+            } else if (abs(i1 - j1) + abs(i2 - j2) == 1){
+                res[i][j] = 2./3;
+            }
         }
     }
     return res;
@@ -162,9 +141,9 @@ double **create_A(int size)
 
 double *create_b(double h, int size)
 {
-    double *res;
     int i1, i2;
-    res = (double *)malloc(size * size * sizeof(double));
+    double *res = (double *)malloc(size * size * sizeof(double));
+
     for (int i = 0; i < size * size; i++) {
         i1 = i / size + 1;
         i2 = i % size + 1;
@@ -183,17 +162,13 @@ double *alternate_triangular_method(double **A, double *b, double h, int size)
     double tau =  2 * h / M_PI;
     double **EwR;
     double *phi;
-    double *sol_prev;
-    double *sol_mid;
-    double *sol_next;
-    double N = size * size;
-    int t;
+    double *sol_prev, *sol_mid, *sol_next;
+    int N = size * size;
 
     phi = (double *)malloc(N * sizeof(double));
     sol_prev = (double *)malloc(N * sizeof(double));
     sol_mid = (double *)malloc(N * sizeof(double));
     sol_next = (double *)malloc(N * sizeof(double));
-
     EwR = create_EwR(A, omega, N);
 
     do {
@@ -203,29 +178,7 @@ double *alternate_triangular_method(double **A, double *b, double h, int size)
         calc_phi(EwR, A, sol_prev, b, phi, tau, N);
         upper_triangle_solve(EwR, phi, sol_mid, N);
         lower_triangle_solve(EwR, sol_mid, sol_next, N);
-
-        /*printf("SOL_PREV~~~~~~~~~~~~~~~~~\n");
-        for (int i = 0; i < N; i++) {
-            printf("%lf\n", sol_prev[i]);
-        }
-        printf("~~~~~~~~~~~~~~~~~");
-
-        printf("SOL_NEXT~~~~~~~~~~~~~~~~~\n");
-        for (int i = 0; i < N; i++) {
-            printf("%lf\n", sol_next[i]);
-        }
-        printf("~~~~~~~~~~~~~~~~~");
-        scanf("%d", &t);*/
     } while (norm(sol_prev, sol_next, size) > EPS);
-
-    /*printf("________________________\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%lf ", EwR[i][j]);
-        }
-        printf("\n");
-    }
-    printf("________________________\n");*/
 
     free(phi);
     free(sol_prev);
@@ -234,58 +187,7 @@ double *alternate_triangular_method(double **A, double *b, double h, int size)
         free(EwR[i]);
     }
     free(EwR);
-
-    /*for (int i = 0; i < size; i++) {
-        sol_next[i] = 3;
-    }*/
     return sol_next;
-}
-
-int choleskyMethod(double **M, const double *B, double *X, int n)
-{
-    double c;
-    double *L;
-    double *Y;
-    L = (double *)malloc(n * n * sizeof(double));
-    Y = (double *)malloc(n * sizeof(double));
-    for (int i = 0; i < n; i++) {
-        c = 0;
-        for (int j = 0; j < i; j++) {
-            c += L[i * n + j] * L[i * n + j];
-            L[j * n + i] = 0.;
-        }
-        if (M[i][i] - c < EPS) {
-            free(Y);
-            free(L);
-            printf("The matrix is not positive definite!\n");
-            return -4;
-        }
-        L[i * n + i] = sqrt(M[i][i] - c);
-        for (int j = i + 1; j < n; j++) {
-            c = 0;
-            for (int k = 0; k < i; k++) {
-                c += L[i * n + k] * L[j * n + k];
-            }
-            L[j * n + i] = (M[j][i] - c) / L[i * n + i];
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        c = 0;
-        for (int j = 0; j < i; j++) {
-            c += Y[j] * L[i * n + j];
-        }
-        Y[i] = (B[i] - c) / L[i * n + i];
-    }
-    for (int i = n - 1; i > -1; i--) {
-        c = 0;
-        for (int j = n - 1; j > i; j--) {
-            c += X[j] * L[j * n + i];
-        }
-        X[i] = (Y[i] - c) / L[i * n + i];
-    }
-    free(L);
-    free(Y);
-    return 0;
 }
 
 double calc_error(double *y, double h, int size)
@@ -318,15 +220,7 @@ int main()
     h = 1. / (n - 1);
     A = create_A(n - 2);
     b = create_b(h, (n - 2));
-    z = (double *)malloc((n - 2) * (n - 2) * sizeof(double));
-    y = (double *)malloc((n - 2) * (n - 2) * sizeof(double));
     z = alternate_triangular_method(A, b, h, n - 2);
-    choleskyMethod(A, b, y, (n - 2) * (n - 2));
-
-    for (int i = 0; i < (n - 2) * (n - 2); i++) {
-        printf("%lf || %lf || %lf\n", b[i], z[i], y[i]);
-    }
-    printf("\n");
 
     printf("Error: %.16f\n", calc_error(z, h, n - 2));
 
@@ -347,7 +241,6 @@ int main()
     }
     free(A);
     free(b);
-    free(y);
     free(z);
 
     return 0;
